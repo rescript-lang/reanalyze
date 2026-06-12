06dd2f9f48d6cfd31199ccff198c0e24e6646438
Jono Prest <jjprest@gmail.com>
2026-06-12 15:58:00 +0000

    Add precise functor argument regression

--- examples/regression/src/Functor_argument.ml
+++ examples/regression/src/Functor_argument.ml
@@ -2,8 +2,27 @@ module Ordered = struct
   type t = int
 
   let compare (left : int) right = Stdlib.compare left right
+  let unused () = ()
 end
 
 module Int_set = Set.Make (Ordered)
 
 let use_set () = ignore (Int_set.singleton 1)
+
+module Extra_fields = struct
+  type extension = ..
+  type extension += Extra
+
+  let before () = ()
+end
+
+module Anonymous_set = Set.Make (struct
+  include Extra_fields
+
+  type t = int
+
+  let compare (left : int) right = Stdlib.compare left right
+  let unused () = ()
+end)
+
+let use_anonymous_set () = ignore (Anonymous_set.singleton 1)
--- examples/regression/src/Main.ml
+++ examples/regression/src/Main.ml
@@ -3,4 +3,5 @@ let definitely_dead () = ()
 let () =
   Live_ancestors.use_child ();
   Functor_argument.use_set ();
+  Functor_argument.use_anonymous_set ();
   Local_side_effects.start ()
